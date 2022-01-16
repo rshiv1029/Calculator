0a7d7fd7a1471a7e42bc459488f203ee8d188787
Rithwik <rithwik.shivram@gmail.com>
2022-01-16 14:12:57 -0500

    Calculator functions all work

--- calculator.py
+++ calculator.py
@@ -3,9 +3,26 @@ from string import digits
 from tkinter import *
 # from tkinter.ttk import *
 
+BG_GRAY = "#c7e6fd"
+OP_BLUE = "#c7e6fd"
+BUT_BLUE = "#edf7fe"
+CAMBRIA_DIGIT = ("Cambria", 24, "bold")
+CAMBRIA_OP = ("Cambria", 16, "bold")
+CAMBRIA_ANS = ("Cambria", 40, "bold")
+
+def on_enter(e):
+    e.widget['background'] = BUT_BLUE
+
+    # e.config(background='OrangeRed3', foreground= "white")
+
+def on_leave(e):
+    e.widget['background'] = BG_GRAY
+    # e.config(background= 'SystemButtonFace', foreground= 'black')
+
 class Calculator:
     def __init__(self) -> None:
         # Variables needed
+        self.needClear = False
         self.equation = ""
         self.answer = ""
         self.digits = { 
@@ -49,7 +66,7 @@ class Calculator:
     
     def initButtonsFrame(self):
         # Init the frame to add widgets
-        buttonFrame = Frame(self.mainWindow)
+        buttonFrame = Frame(self.mainWindow, bg=BG_GRAY)
         buttonFrame.pack(expand="True", fill="both")
         return buttonFrame
 
@@ -58,28 +75,35 @@ class Calculator:
     #####################################################
     def displayLabels(self):
         # UPDATE FONT/FORMATTING(FG,BG)LATER
-        eq = Label(self.display, anchor=E, text=self.equation)
+        eq = Label(self.display, anchor=E, text=self.equation, font= CAMBRIA_OP, bg= "#85a8fc")
         eq.pack(expand="True", fill="both")
-        total = Label(self.display, anchor=E, text=self.answer)
+        total = Label(self.display, anchor=E, text=self.answer, font= CAMBRIA_ANS, bg= "#85a8fc")
         total.pack(expand="True", fill="both")
         return total,eq
 
     def initButtons(self) -> None:
         # UPDATE FONT/FORMATTING(FG,BG)LATER
         for digit,gridVal in self.digits.items():
-            button = Button(self.buttons, text=str(digit), borderwidth=0, command=lambda l=digit: self.modifyEquation(l))
+            button = Button(self.buttons, text=str(digit), borderwidth=0, command=lambda l=digit: self.modifyEquation(l), bg=OP_BLUE, font=CAMBRIA_DIGIT)
             button.grid(row=gridVal[0], column=gridVal[1], sticky=NSEW)
-        
-        for info in self.operators.values():
-            button = Button(self.buttons, text=str(info[0]), borderwidth=0, command=lambda l=info[0]: self.modifyEquation(l))
+            button.bind('<Enter>', on_enter)
+            button.bind('<Leave>', on_leave)
+        for digit,info in self.operators.items():
+            button = Button(self.buttons, text=str(info[0]), borderwidth=0, command=lambda l=digit: self.modifyEquation(l), bg=OP_BLUE, font=CAMBRIA_OP)
             button.grid(row=info[1][0], column=info[1][1],sticky=NSEW)
+            button.bind('<Enter>', on_enter)
+            button.bind('<Leave>', on_leave)
         self.initSpecialButtons()
 
     def initSpecialButtons(self) -> None:    
-        equalButton = Button(self.buttons, text='=', borderwidth=0, command=lambda: self.modifyAnswer())
+        equalButton = Button(self.buttons, text='=', borderwidth=0, command=lambda: self.modifyAnswer(), bg=OP_BLUE, font=CAMBRIA_OP)
         equalButton.grid(row=4, column=3, columnspan=2, sticky=NSEW) 
-        clearButton = Button(self.buttons, text='C', borderwidth=0)
-        clearButton.grid(row=0, column=0, columnspan=4, sticky=NSEW) 
+        equalButton.bind('<Enter>', on_enter)
+        equalButton.bind('<Leave>', on_leave)
+        clearButton = Button(self.buttons, text='C', borderwidth=0, command=lambda: self.clear(), bg=OP_BLUE, font=CAMBRIA_OP)
+        clearButton.grid(row=0, column=0, columnspan=4, sticky=NSEW)
+        clearButton.bind('<Enter>', on_enter)
+        clearButton.bind('<Leave>', on_leave) 
 
     ############################################
     ##          Auxilliary Functions          ##
@@ -91,13 +115,25 @@ class Calculator:
         self.answerLabel.config(text=self.answer)
 
     def modifyEquation(self, digit):
+        if self.needClear:
+            self.needClear = False
+            self.clear()
         self.equation += str(digit)
         self.updateEquationLabel()
     
     def modifyAnswer(self):
         self.answer = eval(self.equation)
         self.updateAnswerLabel()
-
+        self.needClear = True
+    
+    def clear(self):
+        self.equation = ""
+        self.answer = ""
+        self.updateAnswerLabel()
+        self.updateEquationLabel()
+    
+    
+    
     def run(self):
         self.mainWindow.mainloop()
 
